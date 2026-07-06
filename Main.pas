a3e00b2abfc9d738f47bbb184881517ee7519e1b
sigmak <dwfree74@gmail.com>
2026-07-06 15:03:24 +0900

    Ver-0-21
    Stage21: 핸들러에서 sender/e 매개변수 실제 사용

--- Main.pas
+++ Main.pas
@@ -15,7 +15,7 @@ uses
 
 const
   TestSource =
-    'program EventSubscribeTest;' + #10 +
+    'program HandlerParamsTest;' + #10 +
     'type' + #10 +
     '  TMyForm = class(System.Windows.Forms.Form)' + #10 +
     '  public' + #10 +
@@ -28,14 +28,16 @@ const
     'procedure TMyForm.Button1_Click(sender: System.Object; e: System.EventArgs);' + #10 +
     'begin' + #10 +
     '  ClickCount := ClickCount + 1;' + #10 +
-    '  writeln(''버튼 클릭 핸들러 호출됨! ClickCount='' + IntToStr(ClickCount));' + #10 +
+    '  writeln(''핸들러 호출됨. sender.ToString = '' + sender.ToString);' + #10 +
+    '  writeln(''ClickCount='' + IntToStr(ClickCount));' + #10 +
     'end;' + #10 +
     '' + #10 +
     'procedure TMyForm.Setup;' + #10 +
     'begin' + #10 +
     '  Button1 := System.Windows.Forms.Button.Create;' + #10 +
     '  Button1.Click += Button1_Click;' + #10 +
-    '  writeln(''완료: Button1.Click 이벤트 구독 성공 (예외 없음)'');' + #10 +
+    '  Button1_Click(Button1, System.EventArgs.Create);' + #10 +
+    '  writeln(''완료: 핸들러에서 sender 매개변수 사용 성공'');' + #10 +
     'end;' + #10 +
     '' + #10 +
     'var' + #10 +
@@ -51,7 +53,7 @@ var
   codegen: TCodeGenerator; outputName: string;
 
 begin
-  Writeln('=== Stage 20: 이벤트 구독 (Button1.Click += Button1_Click;) ===');
+  Writeln('=== Stage 21: 핸들러에서 sender/e 매개변수 실제 사용 ===');
   Writeln('--- 입력 소스 ---'); Writeln(TestSource); Writeln;
 
   try
@@ -64,7 +66,7 @@ begin
     Writeln('[2/3] 구문분석 완료: 클래스 '+prog.ClassDecls.Count.ToString
       +'개, 메서드구현 '+prog.MethodImpls.Count.ToString+'개');
 
-    outputName:='EventSubscribe_Test_Stage20.exe';
+    outputName:='HandlerParams_Test_Stage21.exe';
     codegen:=new TCodeGenerator(prog);
     codegen.AddReferenceAssembly('System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089');
     codegen.GenerateExe(outputName);
@@ -72,13 +74,11 @@ begin
 
     Writeln;
     Writeln('=====================================================');
-    Writeln('성공! "'+outputName+'" 을 실행하면 다음이 출력되어야 합니다:');
-    Writeln('  완료: Button1.Click 이벤트 구독 성공 (예외 없음)');
+    Writeln('성공! "'+outputName+'" 을 실행하면 다음과 비슷하게 출력됩니다:');
+    Writeln('  핸들러 호출됨. sender.ToString = System.Windows.Forms.Button, Text:');
+    Writeln('  ClickCount=1');
+    Writeln('  완료: 핸들러에서 sender 매개변수 사용 성공');
     Writeln('=====================================================');
-    Writeln;
-    Writeln('참고: 실제로 버튼을 클릭해 핸들러가 호출되는지 보려면');
-    Writeln('Application.Run(f)로 창을 띄우고 버튼을 눌러봐야 확인됩니다');
-    Writeln('(이번 테스트는 "구독 자체가 예외 없이 되는지"만 확인).');
   except
     on E: Exception do
     begin
